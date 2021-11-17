54031dab277f02e78646efca72d0a84e4a6eb0ea
Erika_Toliao <eritol22@bergen.org>
2021-11-17 14:47:05 -0500

    done with blog section

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-#from .forms import PostForm
+from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
