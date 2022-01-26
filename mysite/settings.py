3d4bec6c85b6dd0537e8555a7e54a5b6a4d165f1
Erika_Toliao <eritol22@bergen.org>
2022-01-26 11:29:49 -0500

    added a homepage, fixed the urls to the blogs page, added buttons that redirect to the blogs and pictures pages, and made it pretty

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'pictures.apps.PicturesConfig'
+    'pictures.apps.PicturesConfig',
+    'homepage.apps.HomepageConfig',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('homepage.urls')),
+    path('blog/', include('blog.urls')),
     path('pictures/', include('pictures.urls')),
 ]
 
