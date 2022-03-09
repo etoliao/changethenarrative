5fbd4d4571d02092f8982c73be4032b9051dac10
Erika_Toliao <eritol22@bergen.org>
2022-03-09 11:16:39 -0500

    added debug stuff to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ["DEBUG"] == "True":
+    DEBUG = True
+elif os.environ["DEBUG"] == "False":
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
