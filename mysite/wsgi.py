984bdc77d560b665d23f25cded34d4bc1d867d16
Erika_Toliao <eritol22@bergen.org>
2022-03-09 10:42:36 -0500

    fixed some security things up

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser('~/changethenarrative')
+load_dotenv(os.path.join('.env'))
+SECRET_KEY = os.getenv("SECRET_KEY")
